5ca1453fc11d307ec426401c2c5753d7e7bbe2e6
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2015-10-29 16:38:12 +0100

    [BOOTSTRAP]
    Integrato bootstrap tramite static files, integrata inoltre
    bootstrap-table, un plugin di bootstrap che rende le tabelle più semplici
    da realizzare.
    Per fare ciò ho creato un template di base in SegreteriaInventario/prova/templates/base.html
    dove ho incorporato tutti i css ed i js ed ho copia-incollato uno dei template di base di
    bootstrap che fornisce la navbar.
    Tutti gli altri template andranno ad estendere quello di base, un esempio è quello che si trova in
    SegreteriaInventario/prova/templates/prova/prova.html
    
    [VIEWS]
    
    Ho modificato la view implementando una funzione che ritorna un dizionario alla view
    in modo da poter stampare tutte le colonne che compongono le righe ritornate dalla query

--- SegreteriaInventario/prova/views.py
+++ SegreteriaInventario/prova/views.py
@@ -3,16 +3,35 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.db import connection
 
+# prende il cursore che ha eseguito la query, estrae i risultati sotto
+# forma di dizionario che ha come chiave il nome del campo
+def rows_to_dict_list(cursor):
+    columns = [i[0] for i in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor]
+
 def prova(request):
-    """
-    """
 
     cursor = connection.cursor()
 
-    cursor.execute("select V_IE_CO_INVENTARIO_BENI.DS_BENE, V_IE_CO_INVENTARIO_BENI.ID_INVENTARIO_BENI, V_IE_CO_MOVIMENTI_BENE.DT_REGISTRAZIONE_BUONO, V_IE_CO_INVENTARIO_BENI.VALORE_CONVENZIONALE, V_IE_CO_INVENTARIO_BENI.CD_UBICAZIONE, V_IE_CO_INVENTARIO_BENI.DT_INI_AMMORTAMENTO from V_IE_CO_MOVIMENTI_BENE inner join V_IE_CO_INVENTARIO_BENI on V_IE_CO_MOVIMENTI_BENE.ID_INVENTARIO_BENI = V_IE_CO_INVENTARIO_BENI.ID_INVENTARIO_BENI where V_IE_CO_INVENTARIO_BENI.ID_INVENTARIO_BENI = V_IE_CO_MOVIMENTI_BENE.ID_INVENTARIO_BENI and V_IE_CO_INVENTARIO_BENI.CD_UBICAZIONE='UBI.AMMIN_036' order by V_IE_CO_INVENTARIO_BENI.VALORE_CONVENZIONALE DESC")
+    cursor.execute(
+    	"SELECT \
+	    	INV.ID_INVENTARIO_BENI,\
+	    	INV.DS_BENE,\
+	    	MOV.DT_REGISTRAZIONE_BUONO,\
+	    	INV.VALORE_CONVENZIONALE,\
+	    	SPA.DS_SPAZIO,\
+	    	INV.DT_INI_AMMORTAMENTO\
+    	FROM\
+    		((V_IE_CO_MOVIMENTI_BENE MOV INNER JOIN V_IE_CO_INVENTARIO_BENI INV\
+    		ON MOV.ID_INVENTARIO_BENI = INV.ID_INVENTARIO_BENI) INNER JOIN\
+    		V_IE_AC_SPAZI SPA ON INV.CD_UBICAZIONE = SPA.CD_SPAZIO)\
+    	WHERE\
+    		rownum < 10\
+    	ORDER BY\
+    		INV.VALORE_CONVENZIONALE DESC"
+    	)
     
-    #cursor.execute("select * from V_IE_CO_MOVIMENTI_BENE where rownum<20")
-    rows = cursor.fetchall()
+    rows = rows_to_dict_list(cursor)
 
     context = {'rows': rows}
     
