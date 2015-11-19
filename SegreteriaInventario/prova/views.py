a9b9960fc008991395f615ce649ade2836ce3af3
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2015-11-19 18:05:27 +0100

    [Table Pagination] Finita la paginazione lato server dei dati da mostrare nella tabella localdb, implementata la ricerca con il LIKE case insensitive su tutti i campi presenti fino ad ora

--- SegreteriaInventario/prova/views.py
+++ SegreteriaInventario/prova/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.db import connections
+from django.db.models import Q
 from django.shortcuts import redirect
 from models import Item
 import datetime
+import json
 
 def index(request):
 	return render (request, 'prova/index.html')
@@ -197,6 +199,77 @@ def showSingleItem(request, local_id):
     return render (request, 'prova/singleItem.html', context)
 
 
-def getData(request, order, limit, offset):
-    html = "prova"
+def getData(request):
+    
+    # get string parameters from the url
+    requestOrder = request.GET.get('order', None)
+    sort = request.GET.get('sort', None)
+    search = request.GET.get('search', None)
+    # get numeric parameter if they are defined in the url to avoid None conversion exceptions
+    # if limit is not defined in the url it is set as the number of objects in the database
+    limit = int(request.GET.get('limit')) if (request.GET.get('limit') is not None) else Item.objects.count()
+    offset = int(request.GET.get('offset')) if (request.GET.get('offset') is not None) else 0
+
+    # the number of object retrieved, default value is 0, will be calculated afterwards
+    total = 0
+
+    # if sort is defined the order must be according to the field specified
+    if sort is not None:
+        # if the order is asc we must prepend a "-" to the sort (ex. order by item_id asc -> order by -item_id)
+        order = sort if requestOrder == 'asc' else '-' + sort
+    else:
+        # default is order by item_id asc
+        order = '-item_id'
+
+    # if search is defined we filter the results with the case-insensitive LIKE statement
+    # of all the fields of the model
+    if search is not None:
+
+        # counts the number of objects that match the query
+        total = Item.objects.using('default').filter(\
+            Q(id__icontains= search) | \
+            Q(item_id__icontains= search) | \
+            Q(description__icontains = search) | \
+            Q(purchase_date__icontains = search) | \
+            Q(price__icontains = search) | \
+            Q(location__icontains = search) | \
+            Q(depreciation_starting_date__icontains = search) \
+            ).count()
+
+        # retrieve the objects that match the query
+        rows = Item.objects.using('default').filter(\
+            Q(id__icontains= search) | \
+            Q(item_id__icontains= search) | \
+            Q(description__icontains = search) | \
+            Q(purchase_date__icontains = search) | \
+            Q(price__icontains = search) | \
+            Q(location__icontains = search) | \
+            Q(depreciation_starting_date__icontains = search) \
+            ).order_by(order)[offset:offset + limit]
+
+    else:
+
+        # counts the number of objects
+        total = Item.objects.using('default').all().count()
+
+        # retrieve the objects
+        rows = Item.objects.using('default').all().order_by(order)[offset:offset + limit]
+    
+    # we construct the JSON response, we use json.dumps() to excape undesired characters
+    html = '{ "total": ' + json.dumps(str(total)) + ', "rows": [ '
+    for row in rows:
+        html = html + '{ \
+        "id": ' + json.dumps(str(row.id)) + ', \
+        "item_id": ' + json.dumps(str(row.item_id)) + ', \
+        "description": ' + json.dumps(row.description) + ', \
+        "purchase_date": ' + json.dumps(str(row.purchase_date.date())) + ', \
+        "price": ' + json.dumps(str(row.price)) + ', \
+        "location": ' + json.dumps(row.location) + ', \
+        "depreciation_starting_date": ' + json.dumps(str(row.depreciation_starting_date.date())) +  \
+        ' }, '
+
+    # remove last "," character for the last item
+    if rows.count() > 0:
+        html = html[0:len(html)-2]
+    html = html + ' ] }'
     return HttpResponse(html)
